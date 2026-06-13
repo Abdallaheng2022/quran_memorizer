b18290ada1218e2ba4a38970a2f8b264e88a8cfe
Abdallaheng2022 <106319055+Abdallaheng2022@users.noreply.github.com>
2026-06-13 22:11:15 +0300

    Delete quran_memorizer_flutter/quran_android_flutter(2)/qs_android directory

--- quran_memorizer_flutter/quran_android_flutter(2)/qs_android/app_src/android/QuranAdminReceiver.kt
+++
@@ -1,6 +0,0 @@
-package PLACEHOLDER_PACKAGE
-
-import android.app.admin.DeviceAdminReceiver
-
-// مستقبِل مدير الجهاز — لازم لتفعيل صلاحيات القفل (وللـ Device Owner عند الإعداد).
-class QuranAdminReceiver : DeviceAdminReceiver()
